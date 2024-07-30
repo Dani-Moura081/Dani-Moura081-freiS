084f32326c20abad1e8dd123a9ed4db8f6101bde
dani-moura081 <dani.fe.olav.rodrigues@gmail.com>
2024-07-30 11:56:29 -0300

    primeiro commit

---
+++ src/app/index.jsx
@@ -0,0 +1,33 @@
+import './index.scss';
+// import '../../public/assets/fonts/fonts.css'
+
+export default function app() {
+  return(
+    <div>
+      <header className='cabecalho'>
+
+        <div>
+          <img className='logo' src="/assets/images/logo.png" alt="logo do instituto social nossa senhora de Fátima" />
+        </div>
+
+        <div>
+          <ul>
+            <li>inicio</li>
+            <li>sobre</li>
+          </ul>
+        </div>
+
+      </header>
+      <div className='menu'>
+        <h1>Escolha um treino...</h1>
+
+        <div>
+          
+        </div>
+
+      </div>
+
+    </div>
+  )
+
+}
